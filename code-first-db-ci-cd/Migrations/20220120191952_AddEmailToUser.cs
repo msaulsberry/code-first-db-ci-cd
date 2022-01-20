34b3a85898db1e85e1ab4ea769f005c2282d58aa
Mitch Saulsberry <9msaul@gmail.com>
2022-01-20 13:36:47 -0600

    add migration

---
+++ code-first-db-ci-cd/Migrations/20220120191952_AddEmailToUser.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace code_first_db_ci_cd.Migrations
+{
+    public partial class AddEmailToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "User",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "User");
+        }
+    }
+}
