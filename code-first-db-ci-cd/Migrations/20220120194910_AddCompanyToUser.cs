12c412a86725366480b58e4654113ba1fc582e20
Mitch Saulsberry <37089264+msaulsberry@users.noreply.github.com>
2022-01-20 13:54:56 -0600

    add company column (#3)

---
+++ code-first-db-ci-cd/Migrations/20220120194910_AddCompanyToUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace code_first_db_ci_cd.Migrations
+{
+    public partial class AddCompanyToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Company",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Company",
+                table: "User");
+        }
+    }
+}
