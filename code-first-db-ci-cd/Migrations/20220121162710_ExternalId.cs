0a5a337d14ab71e8770741d1df5258b6e0df31dc
Mitch Saulsberry <37089264+msaulsberry@users.noreply.github.com>
2022-01-21 10:30:28 -0600

    add external id column (#4)
    * add external id column
    
    * add migration

---
+++ code-first-db-ci-cd/Migrations/20220121162710_ExternalId.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace code_first_db_ci_cd.Migrations
+{
+    public partial class ExternalId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ExternalId",
+                table: "User",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExternalId",
+                table: "User");
+        }
+    }
+}
